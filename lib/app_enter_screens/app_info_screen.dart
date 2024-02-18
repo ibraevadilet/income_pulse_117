85f5abf75a536ed07e1dbf96c3709d39642184f7
Tolibzhon <sidikovtolibjon2000@gmail.com>
2024-02-18 14:19:08 +0600

    add info screen

--- lib/app_enter_screens/app_info_screen.dart
+++ lib/app_enter_screens/app_info_screen.dart
@@ -1,15 +1,10 @@
-import 'package:apphud/apphud.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:income_pulse_117/app_enter_screens/pulse_web_screen.dart';
+import 'package:income_pulse_117/app_enter_screens/info_cnt.dart';
 import 'package:income_pulse_117/app_helpers/shared_keys.dart';
 import 'package:income_pulse_117/helper_screens/buy_screen.dart';
 import 'package:income_pulse_117/main.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
-
-import '../helper_screens/main_bottom_screen.dart';
 
 class AppInfoScreen extends StatefulWidget {
   final String linkImage;
@@ -22,281 +17,128 @@ class AppInfoScreen extends StatefulWidget {
 class _AppInfoScreenState extends State<AppInfoScreen> {
   final controller = PageController();
   int pageIndex = 0;
+  late List<InfoCnt> tokenInformation;
+  @override
+  void initState() {
+    if (widget.linkImage.isEmpty) {
+      tokenInformation = listInfo;
+    } else {
+      tokenInformation = listInfoPocket;
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: widget.linkImage.isEmpty
+          ? const Color(0xff272727)
+          : const Color(0xff232B45),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.w),
-          child: Column(
-            children: [
-              Expanded(
-                flex: 3,
-                child: Stack(
-                  alignment: Alignment.bottomCenter,
-                  children: [
-                    PageView(
-                      onPageChanged: (value) {
-                        pageIndex = value;
-                      },
-                      controller: controller,
-                      children: [
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 64.w),
-                              child: Image.asset(
-                                'assets/images/on_brd1.png',
-                              ),
-                            ),
-                            SizedBox(height: 43.h),
-                            Text(
-                              'Your Finances\nRecord, Analyze, Plan!',
-                              style: TextStyle(
-                                fontSize: 22.h,
-                                fontFamily: fontFamily,
-                                fontWeight: FontWeight.normal,
-                                color: Colors.white.withOpacity(0.6),
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 64.w),
-                              child: Image.asset(
-                                'assets/images/on_brd2.png',
-                              ),
-                            ),
-                            SizedBox(height: 43.h),
-                            Text(
-                              ' Your Wallet - Record,\nVisualize, Plan Ahead!',
-                              style: TextStyle(
-                                fontSize: 22.h,
-                                fontFamily: fontFamily,
-                                fontWeight: FontWeight.normal,
-                                color: Colors.white.withOpacity(0.6),
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 64.w),
-                              child: Image.asset(
-                                'assets/images/on_brd3.png',
-                              ),
-                            ),
-                            SizedBox(height: 43.h),
-                            Text(
-                              'Elevate\nyour wealth',
-                              style: TextStyle(
-                                fontSize: 22.h,
-                                fontFamily: fontFamily,
-                                fontWeight: FontWeight.normal,
-                                color: Colors.white.withOpacity(0.6),
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                    Positioned(
-                      bottom: 120.h,
-                      child: SmoothPageIndicator(
-                        controller: controller,
-                        count: 3,
-                        effect: WormEffect(
-                          dotWidth: 16.0.w,
-                          dotHeight: 16.w,
-                          spacing: 32.w,
-                          activeDotColor: const Color(0xff1DB954),
-                          dotColor: const Color(0xffD9D9D9),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // const SizedBox(height: 19),
-              // SmoothPageIndicator(
-              //   controller: controller,
-              //   count: 3,
-              //   effect: const WormEffect(
-              //     spacing: 32,
-              //     activeDotColor: Color(0xff1DB954),
-              //     dotColor: Color(0xffD9D9D9),
-              //   ),
-              // ),
-              CupertinoButton(
-                color: Colors.transparent,
-                padding: EdgeInsets.zero,
-                onPressed: () async {
-                  if (pageIndex == 2) {
-                    prefs.setBool(AppSharedKeys.first, true);
-                    Navigator.push(
-                      context,
-                      CupertinoPageRoute(
-                        builder: (context) => const BuyScreen(),
-                      ),
-                    );
-                  } else {
-                    controller.nextPage(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.ease,
-                    );
-                  }
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                itemCount: tokenInformation.length,
+                controller: controller,
+                onPageChanged: (index) {
+                  setState(() {
+                    pageIndex = index;
+                  });
                 },
-                child: Container(
-                  padding: const EdgeInsets.all(14),
-                  alignment: Alignment.center,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: const Color(0xff1DB954),
-                  ),
-                  child: Text(
-                    'Continue',
-                    style: TextStyle(
-                      fontSize: 16.h,
-                      fontFamily: fontFamily,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
+                itemBuilder: ((context, index) => InfoWidget(
+                      model: tokenInformation[index],
+                    )),
+              ),
+            ),
+            SizedBox(height: 24.h),
+            CupertinoButton(
+              color: Colors.transparent,
+              padding: EdgeInsets.zero,
+              onPressed: () async {
+                if (pageIndex == 4) {
+                  prefs.setBool(AppSharedKeys.first, true);
+                  Navigator.push(
+                    context,
+                    CupertinoPageRoute(
+                      builder: (context) => const BuyScreen(),
                     ),
+                  );
+                } else {
+                  controller.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.ease,
+                  );
+                }
+              },
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 16.r),
+                padding: const EdgeInsets.all(14),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: widget.linkImage.isEmpty
+                      ? const Color(0xff1DB954)
+                      : const Color(0xff4E8AFF),
+                ),
+                child: Text(
+                  'Continue',
+                  style: TextStyle(
+                    fontSize: 16.h,
+                    fontFamily: fontFamily,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
                   ),
                 ),
               ),
-              const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 4),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) => const PulseWebScreen(
-                              title: 'Privacy Policy',
-                              url:
-                                  'https://sites.google.com/view/income-pulse/privacy-policy',
-                            ),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        'Privacy Policy',
-                        style: TextStyle(
-                          fontSize: 13.h,
-                          fontFamily: fontFamily,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white.withOpacity(0.6),
-                        ),
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () async {
-                        final hasPremiumAccess =
-                            await Apphud.hasPremiumAccess();
-                        final hasActiveSubscription =
-                            await Apphud.hasActiveSubscription();
-                        if (hasPremiumAccess || hasActiveSubscription) {
-                          final prefs = await SharedPreferences.getInstance();
-                          prefs.setBool(AppSharedKeys.prem, true);
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) =>
-                                CupertinoAlertDialog(
-                              title: const Text('Success!'),
-                              content: const Text(
-                                  'Your purchase has been restored!'),
-                              actions: [
-                                CupertinoDialogAction(
-                                  isDefaultAction: true,
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                    Navigator.pushAndRemoveUntil(
-                                      context,
-                                      CupertinoPageRoute(
-                                        builder: (_) =>
-                                            const MainBottomScreen(),
-                                      ),
-                                      (route) => false,
-                                    );
-                                  },
-                                  child: const Text('Ok'),
-                                ),
-                              ],
-                            ),
-                          );
-                        } else {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) =>
-                                CupertinoAlertDialog(
-                              title: const Text('Restore purchase'),
-                              content: const Text(
-                                  'Your purchase is not found. Write to support: https://sites.google.com/view/income-pulse/support-from'),
-                              actions: [
-                                CupertinoDialogAction(
-                                  isDefaultAction: true,
-                                  onPressed: () =>
-                                      {Navigator.of(context).pop()},
-                                  child: const Text('Ok'),
-                                ),
-                              ],
-                            ),
-                          );
-                        }
-                      },
-                      child: Text(
-                        'Restore',
-                        style: TextStyle(
-                          fontSize: 13.h,
-                          fontFamily: fontFamily,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white.withOpacity(0.6),
-                        ),
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) => const PulseWebScreen(
-                              title: 'Terms of use',
-                              url:
-                                  'https://sites.google.com/view/income-pulse/terms-conditions',
-                            ),
-                          ),
-                        );
-                      },
-                      child: Text(
-                        'Terms of use',
-                        style: TextStyle(
-                          fontSize: 13.h,
-                          fontFamily: fontFamily,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white.withOpacity(0.6),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 8),
-            ],
-          ),
+            ),
+            const SizedBox(height: 50),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class InfoWidget extends StatelessWidget {
+  const InfoWidget({
+    Key? key,
+    required this.model,
+  }) : super(key: key);
+  final InfoCnt model;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.r),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.r),
+            child: Image.asset(model.images),
+          ),
+          SizedBox(height: 30.h),
+          Text(
+            model.title,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 22.h,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 4.h),
+          Text(
+            model.subTitle,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16.h,
+              fontWeight: FontWeight.w400,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/app_enter_screens/info_cnt.dart
@@ -0,0 +1,77 @@
+class InfoCnt {
+  final String images;
+  final String title;
+  final String subTitle;
+
+  InfoCnt({
+    required this.images,
+    required this.title,
+    required this.subTitle,
+  });
+}
+
+List<InfoCnt> listInfo = [
+  InfoCnt(
+    images: 'assets/images/on_brd1.png',
+    title: 'Smart Finance Manager: Control, Analyze, Plan!',
+    subTitle:
+        'Optimize your finances with an innovative tool for recording, analyzing and planning financial transactions!',
+  ),
+  InfoCnt(
+    images: 'assets/images/on_brd2.png',
+    title: 'Your Wallet Companion: Record, Visualize, Plan Ahead',
+    subTitle:
+        'A handy tool to easily record transactions, spending patterns and strategically plan for the future!',
+  ),
+  InfoCnt(
+    images: 'assets/images/on_brd3.png',
+    title: 'Elevate Your\nWealth',
+    subTitle:
+        'Unlock the path to financial prosperity with strategic planning, wise investments, and mindful wealth management.',
+  ),
+  InfoCnt(
+    images: 'assets/images/on_brd4.png',
+    title: 'Stay In the Loop with Push Notifications',
+    subTitle:
+        'Activate Now to Ensure You Never Miss Another Important Announcement!',
+  ),
+  InfoCnt(
+    images: 'assets/images/on_brd5.png',
+    title: 'Shape the\nFuture Together',
+    subTitle:
+        'Contribute Your Insights - Make a Difference in Enhancing Our App Experience!',
+  ),
+];
+
+List<InfoCnt> listInfoPocket = [
+  InfoCnt(
+    images: 'assets/images/on_br1.png',
+    title: 'Unlock Market\nPotentia',
+    subTitle:
+        'Explore a Diverse Range of Assets, Trade Strategically, and Expand Your Opportunities in the Financial Landscape.',
+  ),
+  InfoCnt(
+    images: 'assets/images/on_br2.png',
+    title: 'Elevate Your\nTrading Game',
+    subTitle:
+        'Secure Your Trades in Favorable Conditions and Safeguard Your Investments for Maximum Profitability.',
+  ),
+  InfoCnt(
+    images: 'assets/images/on_br3.png',
+    title: 'Enhance your trading experience with 50% bonus',
+    subTitle:
+        'Level Up Your Trading Potential: Earn More with a Generous +50% Bonus on Your Initial Investment!',
+  ),
+  InfoCnt(
+    images: 'assets/images/on_br4.png',
+    title: 'Unlock Profitable Opportunities',
+    subTitle:
+        'Receive Exclusive and Accurate Trading Signals through Instant Push Notifications – Seize Profitable Opportunities Now!',
+  ),
+  InfoCnt(
+    images: 'assets/images/on_br5.png',
+    title: 'Shape the\nFuture Together',
+    subTitle:
+        ' Contribute Your Insights - Make a Difference in Enhancing Our App Experience!',
+  ),
+];
